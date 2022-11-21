236075a03361f33896e542e6a7dc8e8b14a9ca09
Chetan Menaria <93630545+iTsChEtAn07@users.noreply.github.com>
2022-11-21 22:01:40 +0530

    Create Unique partitions.cpp

---
+++ Unique partitions.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+	vector<vector<int>>ans;
+	void helper(int n,vector<int>&curr,int target){
+	    if(target==0){
+	    ans.push_back(curr);
+	    return;
+	    }
+	    if(n==0)return;
+	    if(target>=n){
+	        curr.push_back(n);
+	        helper(n,curr,target-n);
+	        curr.pop_back();
+	    }
+	    helper(n-1,curr,target);
+	}
+    vector<vector<int>> UniquePartitions(int n) {
+        // Code here
+        vector<int>curr;
+        helper(n,curr,n);
+        return ans;
+    }
+    };
